5ed96e623f167fae09913c30b6bea15bffe82696
George-du <duduo1@163.com>
2021-06-27 01:55:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "心爱的小蹄蹄",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
         "第三个生日了哦",
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "TURN ON LIGHTS",
+        play: "PLAY MUSIC",
+        bannar_coming: "LET'S DECORATE",
+        balloons_flying: "FLY WITH BALLOONS",
+        cake_fadein: "MOST DELICIOUS CAKE EVER",
+        light_candle: "LIGTH CANDLE",
+        wish_message: "HAPPY BIRTHDAY",
         story: "A MESSAGE FOR YOU",
     }
 };
